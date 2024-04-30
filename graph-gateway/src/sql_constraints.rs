5531770f40383c7c684f4eadcd33af28b6489fa3
Joseph Livesey <joseph@semiotic.ai>
2024-04-30 15:41:08 -0400

    feat: invalidate sql query

--- graph-gateway/src/sql_constraints.rs
+++ graph-gateway/src/sql_constraints.rs
@@ -3,92 +3,28 @@ use cost_model::Context;
 use gateway_framework::errors::Error;
 use graphql::graphql_parser::query::OperationDefinition;
 
-pub fn validate_sql_query(ctx: &Context<String>) -> Result<(), Error> {
-    for operation in &ctx.operations {
-        match operation {
-            OperationDefinition::Query(query) => {
-                let items_not_sql = query
-                    .selection_set
-                    .items
-                    .iter()
-                    .filter_map(|selection| {
-                        if let graphql::graphql_parser::query::Selection::Field(field) = selection {
-                            if field.name != "sql" {
-                                return Some(field.name.as_str());
-                            }
-                        }
-                        None
-                    })
-                    .collect::<Vec<_>>();
-                if items_not_sql.len() > 0 {
-                    return Err(Error::BadQuery(anyhow!(
-                        "Fields [{}] are not SQL",
-                        items_not_sql.join(", ")
-                    )));
+pub fn invalidate_sql_query(ctx: &Context<String>) -> Result<(), Error> {
+    if ctx.operations.iter().any(|operation| {
+        if let OperationDefinition::Query(query) = operation {
+            query.selection_set.items.iter().any(|selection| {
+                if let graphql::graphql_parser::query::Selection::Field(field) = selection {
+                    return field.name == "sql";
                 }
-            }
-            _ => continue,
+                false
+            })
+        } else {
+            false
         }
+    }) {
+        return Err(Error::BadQuery(anyhow!("Query contains SQL")));
     }
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_single_selection_set() {
-        let query = r#"
-            query {
-                sql(input: { query: "SELECT * FROM users" }) {
-                    id
-                    name
-                }
-            }
-        "#;
-        let variables = r#"{}"#;
-        let ctx = Context::new(query, variables).unwrap();
-        assert!(validate_sql_query(&ctx).is_ok());
-    }
-
-    #[test]
-    fn test_multi_selection_set() {
-        let query = r#"
-            query {
-                sql(input: { query: "SELECT * FROM users" }) {
-                    rows
-                    columns
-                }
-                sql(input: { query: "SELECT * FROM tokens" }) {
-                    id
-                    name
-                }
-            }
-        "#;
-        let variables = r#"{}"#;
-        let ctx = Context::new(query, variables).unwrap();
-        assert!(validate_sql_query(&ctx).is_ok());
-    }
-
-    #[test]
-    fn test_multi_selection_set_not_sql() {
-        let query = r#"
-            query {
-                sql(input: { query: "SELECT * FROM users" }) {
-                    id
-                    name
-                }
-                users {
-                    id
-                    name
-                }
-            }
-        "#;
-        let variables = r#"{}"#;
-        let ctx = Context::new(query, variables).unwrap();
-        assert!(validate_sql_query(&ctx).is_err());
-    }
 
     #[test]
     fn test_no_sql() {
@@ -106,9 +42,59 @@ mod tests {
         "#;
         let variables = r#"{}"#;
         let ctx = Context::new(query, variables).unwrap();
-        assert_eq!(
-            validate_sql_query(&ctx).err().unwrap().to_string(),
-            Error::BadQuery(anyhow!("Fields [tokens, users] are not SQL")).to_string()
-        );
+        assert!(invalidate_sql_query(&ctx).is_ok());
+    }
+
+    #[test]
+    fn test_single_selection_set() {
+        let query = r#"
+            query {
+                sql(input: { query: "SELECT * FROM users" }) {
+                    id
+                    name
+                }
+            }
+        "#;
+        let variables = r#"{}"#;
+        let ctx = Context::new(query, variables).unwrap();
+        assert!(invalidate_sql_query(&ctx).is_err());
+    }
+
+    #[test]
+    fn test_multi_selection_set_with_sql() {
+        let query = r#"
+            query {
+                sql(input: { query: "SELECT * FROM users" }) {
+                    id
+                    name
+                }
+                users {
+                    id
+                    name
+                }
+            }
+        "#;
+        let variables = r#"{}"#;
+        let ctx = Context::new(query, variables).unwrap();
+        assert!(invalidate_sql_query(&ctx).is_err());
+    }
+
+    #[test]
+    fn test_multi_sql_fields() {
+        let query = r#"
+            query {
+                sql(input: { query: "SELECT * FROM tokens" }) {
+                    id
+                    name
+                }
+                sql(input: { query: "SELECT * FROM users" }) {
+                    id
+                    name
+                }
+            }
+        "#;
+        let variables = r#"{}"#;
+        let ctx = Context::new(query, variables).unwrap();
+        assert!(invalidate_sql_query(&ctx).is_err());
     }
 }
